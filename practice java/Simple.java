91f2ea4d48ff51f33693f7ed4c5bfe734256007c
malithweerasinghe <malithrashan03@gmail.com>
2020-11-25 18:11:03 +0530

    first commit

---
+++ practice java/Simple.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.util.Scanner;
+class Simple{  
+    public static void main(String []args){  
+    /* int x,y,z;
+        x=10;
+        y=20;
+        z=30;
+        System.out.println(x); //prints 10
+        System.out.println(y); //prints 20
+        System.out.println(z); //prints 30
+
+       
+        y=300;
+        System.out.println(y); //*/
+        
+        
+        
+        int r=100;
+        int t=200;
+        System.out.println(r);  //Prints 100
+        System.out.println(t);  //prints 200
+        
+        t=r; //copy value of y to x
+        System.out.println(r); //Prints ?
+        System.out.println(t); //Prints ? 
+        
+       /* int x=100;
+        int y=200;
+        System.out.println(x);  //Prints 100
+        System.out.println(y);  //prints 200
+        
+        x=y; //copy value of y to x
+        System.out.println(x); //Prints ?
+        System.out.println(y); //Prints ? */
+        
+        System.out.println("AB+CD");  //ABCD -->String concat
+        System.out.println("10+20"); //30 -->Arithmetic Addition
+
+
+
+
+    }  
+}
---
+++ practice java/ex1.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.util.Scanner;
+
+class ex1{
+      public static void main(String[]args){
+            System.out.println("Rashan Malith");
+            System.out.println("Hello Rashan");
+            System.out.println("Hello Malith");
+            System.out.print("N");
+        System.out.print("No 30,");
+        System.out.print("Walana Road,");    
+        System.out.print("Panadura.");    
+        System.out.print("A");
+        System.out.print("B");
+       // System.out.println("C");
+        System.out.print("D");
+        //System.out.println("E");
+        System.out.println("A");
+       /* System.out.println("B");*/
+     System.out.print("A");
+        System.out.println();
+        System.out.print("B");
+        System.out.println();
+        System.out.print("C");
+        System.out.println();
+        
+        System.out.println(100); //prints 100 --> Decimal
+        System.out.println(0b1100100);//prints 100 -->Binary
+        System.out.println(0144);//prints 100 -->Octal
+        System.out.println(0x64);//prints 100 -->Hexadecimal
+        
+        System.out.println(0b1100100);
+        System.out.println(0B1100100);
+        System.out.println(1100100); //Decimal
+
+        //System.out.println(0b1100200); //Illegal
+        
+        System.out.println(true); 
+        System.out.println(false); 
+        System.out.println(true);  //Illegal
+        System.out.println(false);  //Illegal
+        
+        System.out.println("ABCD");
+        System.out.println("AB\tCD"); //\t-->tab
+        System.out.println("AB\bCD"); //\b-->Backspace
+        System.out.println("AB\nCD"); //\n-->new line (Enter key)
+        
+        int x; //Create a variable (memory location) named “x”
+        x=100; //assign 100 to variable x
+        System.out.println(x); //prints value of variable x
+
+
+
+
+
+
+
+
+
+      }
+ 
+}
