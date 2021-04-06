6dafe8511b7aeaab1dde3a174761a37442ba8a0e
malithweerasinghe <malithrashan03@gmail.com>
2021-04-06 15:49:09 +0530

    oop practice

---
+++ test/program1.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class program1 {
+
+    public static void main(String ar[] ){
+
+
+      Scanner sc= new Scanner(System.in);
+
+int x;
+
+x=sc.nextInt();
+System.out.print(" NUmber is = "+(--x));
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
