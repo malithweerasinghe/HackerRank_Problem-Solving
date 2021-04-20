226142f0e79cb0cd9b50ac301f763a6e2bbb60e2
RashanWeerasinghe <malithrashan03@gmail.com>
2021-04-20 15:28:04 +0530

    this keyword

--- test/Demo.java
+++ test/Demo.java
@@ -1,39 +1,29 @@
-class Myclass{
-
-   byte a;
-   short b;
-   int c;
-   long d;
-   float e;
-   double f;
-   char g;
-
-  boolean h;
-
-  Box b1;
-  string name;
-  int[] xr;
-  Random r;
-
-}
-
-
-public class Demo {
-
-    public static void main(String args[]){
-        MyClass c1=new MyClass();
-        System.out.println("byte  : "+c1.a);
-        System.out.println("short : "+c1.b);
-        System.out.println("int   : "+c1.c);
-        System.out.println("long  : "+c1.d);
-        System.out.println("float : "+c1.e);
-        System.out.println("double: "+c1.f);
-        System.out.println("char  : "+c1.g);
-        System.out.println("boolean: "+c1.h);
-        
-        System.out.println("Box   : "+c1.b1);
-        System.out.println("String: "+c1.name);
-        System.out.println("int[] : "+c1.xr);
-        System.out.println("Random: "+c1.r);
+class Box{
+    int length;
+    int width;
+    int height;
+    
+    public void printVolume(){
+        int volume;
+        volume=length*width*height;
+        System.out.println("Volume of the box is : "+volume);
+    }
+    public void setSize(int length, int width, int height){
+         this.length=length;
+         this.width=width;
+         this.height=height;
     }
 }
+class Demo{
+    public static void main(String args[]){
+        Box b1=new Box();
+        b1.setSize(12,5,3);
+        
+        System.out.println("Length of b1 object : "+b1.length); //0
+        System.out.println("Width  of b1 object : "+b1.width);    //0
+        System.out.println("Height of b1 object : "+b1.height);    //0
+        b1.printVolume();    //0
+    }
+}
+
+
