ccb6a39796a25fd912be651bef7d1c5d60cf60ca
RashanWeerasinghe <malithrashan03@gmail.com>
2021-04-20 11:39:24 +0530

    error fix

---
+++ test/Window.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+
+
+public class Window {
+    
+public static void main(String args[]){
+   JFrame f1;
+   f1=new JFrame();
+
+
+   f1.setSize(300,300);
+   f1.setTitle("calculator");
+   f1.setDefaultCloseOperation(3);
+   f1.setLocationRelativeTo(null);
+   f1.setVisible(true);
+
+}
+
+
+}
--- test/myjava1.java
+++ test/myjava1.java
@@ -5,10 +5,5 @@ public static void main(String []args){
    System.out.println("Hello World");
    System.out.println("Hello World");
 }
-}
-}
-
-
-
 
 }
