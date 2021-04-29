a6f37634c3f89e17c1916198bbf5614209944d46
RashanWeerasinghe <malithrashan03@gmail.com>
2021-04-29 15:11:41 +0530

    constructor overload

--- test/Demo.java
+++ test/Demo.java
@@ -3,23 +3,38 @@ class Box{
     int width;
     int height;
     
-    Box(){
-        length=0;
-        width=0;
-        height=0;
+    public Box(int length, int width, int height){ //Parameterized constructor
+        this.length=length;
+        this.width=width;
+        this.height=height;
+    }    
+    public Box(int length){ //Parameterized constructor
+        this.length=length;
+        this.width=length;
+        this.height=length;
+    }    
+    public Box(){
+        length=1;
+        width=1;
+        height=1;
     }
-   
+    public void printVolume(){
+        int volume;
+        volume=length*width*height;
+        System.out.println("Volume of the box is : "+volume);
+    }
+
 }
+
 class Demo{
     public static void main(String args[]){
-        Box b1=new Box();
-       
+        Box b1=new Box(12,5,3);
+        Box b2=new Box(10);
+        Box b3=new Box();
+
+        b1.printVolume();
+        b2.printVolume();
+        b3.printVolume();
         
-        System.out.println("Length of b1 object : "+b1.length); //0
-        System.out.println("Width  of b1 object : "+b1.width);    //0
-        System.out.println("Height of b1 object : "+b1.height);    //0
-      
     }
 }
-
-
